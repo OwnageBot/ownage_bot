91fd75b15421b0877c0feceba4022db7e7b253aa
Tan Zong Xuan <tanqazx@gmail.com>
2017-03-03 12:20:20 -0500

    Fixed directory structure.

--- src/object_tracker.cpp
+++
@@ -1,9 +0,0 @@
-#include "object_tracker.h"
-
-using namespace std;
-using namespace baxter_core_msgs;
-
-ObjectTracker::ObjectTracker(std::string _name, std::string _limb, bool _no_robot) :
-                            ARucoClient(_name, _limb), elap_time(0)
-{
-}
