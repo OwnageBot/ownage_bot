eab0c6c6f5729839182f64caa43ac5b6b2bfc3e7
Tan Zong Xuan <tanqazx@gmail.com>
2017-04-09 20:42:47 -0400

    Add scaffolding code for feedback and classification.

--- src/object_collector/object_collector.py
+++ src/object_collector/object_collector.py
@@ -12,6 +12,7 @@ class ObjectCollector:
     """A class for collecting objects."""
 
     def __init__(self):
+        self.skipScan = False
         # Threshold for forbiddenness
         self.threshold = (rospy.get_param("collect_threshold") if
                           rospy.has_param("collect_threshold") else 0.8)
@@ -24,11 +25,6 @@ class ObjectCollector:
         self.actionProvider = rospy.ServiceProxy(
             "/action_provider/service_left", DoAction)
 
-    def objectDbCallback(self):
-        """Callback upon receiving updated database from ObjectTracker."""
-        # Interrupt search and pick up new objects
-        pass
-
     def scanWorkspace(self):
         return self.actionProvider("scan", [DUMMY_OBJ])
 
@@ -40,32 +36,52 @@ class ObjectCollector:
 
     def putDown(self):
         return self.actionProvider("put", [DUMMY_OBJ])
-
+    
     def collect(self, obj):
-        resp = self.pickUp(obj)
-        if not resp.success:
-            return resp
-        resp = self.goHome()
-        if not resp.success:
-            return resp
-        resp = self.putDown()
-        if not resp.success:
-            return resp        
+        """Attempts to bring object to home area."""
+        ret = self.pickUp(obj)
+        if not ret.success:
+            return ret
+        ret = self.goHome()
+        if not ret.success:
+            return ret
+        ret = self.putDown()
+        if not ret.success:
+            return ret
     
     def inHomeArea(self, obj):
+        """Checks if object is in home area."""
         loc = obj.pose.pose.position
         return ((self.home_area[0].x <= loc.x <= self.home_area[1].x) and
                 (self.home_area[0].y <= loc.y <= self.home_area[1].y))
-        
+
+    def classify(self, objects):
+        """Requests ObjectClassifier to determine if objects are owned."""
+        return objects
+
+    def blacklist(self, obj):
+        """Tells classifier to blacklist given object and update ownership."""
+        pass
+    
     def main(self):
         """Main loop which collects all tracked objects."""
         while not rospy.is_shutdown():
-            self.scanWorkspace()
+            if self.skipScan:
+                self.skipScan = False
+            else:
+                self.scanWorkspace()
+            # Get list of objects and classify their ownership
             msg = rospy.wait_for_message("object_db", RichObjectArray)
-            for obj in msg.objects:
+            objects = self.classify(msg.objects)
+            for obj in objects:
                 if (not self.inHomeArea(obj) and
                     obj.forbiddenness < self.threshold):
-                    self.collect(obj)                    
+                    ret = self.collect(obj)
+                    if ret.response == DoAction.ACT_FAILED:
+                        # Update ownership rules and reclassify
+                        self.blacklist(obj)
+                        self.skipScan = True
+                        break
 
 if __name__ == '__main__':
     rospy.init_node('object_collector')
