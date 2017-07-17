906dcfe3a957f310fd320b88496bb0466ac204cd
ztangent <tanqazx@gmail.com>
2017-07-17 16:45:46 -0400

    Added Area class and inArea method.

--- src/ownage_bot/Objects.py
+++ src/ownage_bot/Objects.py
@@ -1,6 +1,8 @@
 import math
 import copy
 import rospy
+import numpy as np
+import matplotlib.path as mptPath
 from ownage_bot.msg import RichObject
 from geometry_msgs.msg import Point, Quaternion
 
@@ -19,7 +21,7 @@ class Object:
             self.is_avatar = False # Whether object is an avatar
             self.is_landmark = False # Whether object is a landmark
         else:
-            uncopyable = ["owners, "ownership"] 
+            uncopyable = ["owners", "ownership"] 
             for k, v in msg.__dict__.items():
                 if k in uncopyable:
                     continue
@@ -29,6 +31,7 @@ class Object:
             self.ownership = dict(zip(obj_msg.owners, obj_msg.ownership))
             
     def asMessage(self):
+        """Converts object to a ROS message."""
         msg = RichObject()
         uncopyable = ["ownership"] 
         for k, v in self.__dict__.items():
@@ -39,9 +42,21 @@ class Object:
         msg.ownership = self.ownership.values()
         return msg
 
+class Area:
+    """Defines a 2D polygonal area."""
+    def __init__(self, points):
+        """Takes a list of tuples and stores them."""
+        self.n_sides = len(points)
+        self.points = np.array(points)
+        self.path = mptPath.Path(self.points)
+                                                    
 def dist(obj1, obj2):
     """Calculates Euclidean distance between two objects."""
     p1 = obj1.position
     p2 = obj2.position
     diff = [p1.x-p2.x, p1.y-p2.y, p1.z-p2.z]
     return math.sqrt(sum([d*d for d in diff]))
+
+def inArea(obj, area):
+    """Checks if object is located in area."""
+    return area.path.contains_point((obj.position.x, obj.position.y))
