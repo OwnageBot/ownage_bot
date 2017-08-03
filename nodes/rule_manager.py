0caf3c8bbeb732bca462ecf079bdcd5a35496eeb
Tan Zong Xuan <tanqazx@gmail.com>
2017-08-03 17:25:11 -0400

    Reorganized code in RuleManager a little.

--- nodes/rule_manager.py
+++ nodes/rule_manager.py
@@ -58,11 +58,13 @@ class RuleManager:
         if len(fact.args) != 1:
             raise TypeError("Action fact should have exactly one argument.")
         tgt = fact.args[0]
+
         # Make sure target is represented in a hashable way
         if action.tgtype == Object:
             tgt = int(tgt)
         elif action.tgtype == Point:
             tgt = tuple(tgt.split())
+
         # Overwrite old value if fact already exists
         self.fact_db[action.name][tgt] = fact.confidence
 
@@ -70,54 +72,68 @@ class RuleManager:
         """Updates candidate rule database, considers activating them."""
         action = self.action_db[msg.action]
         rule = Rule.fromMsg(msg)
+
         # Overwrites old value if candidate already exists
         self.cand_rule_db[action.name][rule] = msg.confidence
         self.updateRuleSet(action.name, rule)
 
     def updateRuleSet(self, act_name, cand_rule=None):
         """Evaluates rules for the named action, updates if necessary."""
+
+        if len(self.fact_db[act_name]) > 0:
+            # Grow rule set (e.g. add / refine candidate rules)
+            self.growRuleSet(act_name, cand_rule)
+        elif cand_rule is not None:
+            # If there are no facts, just use candidate rules
+            self.fact_db[act_name].add(cand_rule)
+
+        # Prune ruleset (e.g. merge rules that can be merged)
+        self.pruneRuleSet(act_name, cand_rule)
+
+    def growRuleSet(self, act_name, cand_rule=None):
+        """Grow rule set via modified ProbFOIL algorithm."""
         rule_set = self.active_rule_db[act_name]
         fact_set = self.fact_db[act_name]
-
-        # If there are no facts to check against, just use candidate rules
-        if len(fact_set) == 0:
-            if cand_rule is not None:
-                rule_set.add(cand_rule)
-            return
-
+        
         # Consider adding rules if accuracy is too low
         perf = self.evalRuleSet(rule_set, fact_set)
         while perf.acc < 0.9:
-            if cand_rule is None:
-                new_rule = self.guessRule(act_name)
-            else:
-                new_rule = cand_rule
+            # Start from candidate rule if available
+            new_rule = (cand_rule if cand_rule not is None else
+                        self.guessRule(act_name))
 
-            # Add new rule and evaluate improvement
+            # Repeatedly evaluate and refine rule
             while True:
+                # Skip if rule is already in ruleset
                 if new_rule in rule_set:
-                    self.refineRule(new_rule)
+                    new_rule = self.refineRule(new_rule)
                     continue
+                # Add rule and evaluate performance
                 rule_set.add(new_rule)
                 new_perf = self.evalRuleSet(rule_set, fact_set)
-
                 # Check if stopping criterion is true
                 if new_perf.fp == 0 or (new_perf.tp - perf.tp == 0):
-                    rule_set.remove(new_rule)
                     break
-
-                # Refine rule (shoud select best refinement)
+                # Refine rule (should select best refinement)
+                rule_set.remove(new_rule)
                 new_rule = self.refineRule(new_rule)
 
-            # Break if accuracy does not improve, else keep going
+            # Break if accuracy does not improve, else keep adding more
             new_perf = self.evalRuleSet(rule_set, fact_set)
             if new_perf.acc <= perf.acc:
+                # Get rid of rule added in inner loop
                 rule_set.remove(new_rule)
                 break
 
-        # Prune ruleset (e.g. merge rules that can be merged)
-        self.pruneRuleSet(rule_set, fact_set)
-
+    def pruneRuleSet(self, act_name, cand_rule=None):
+        """Prunes the active ruleset for the named action."""
+        rule_set = self.active_rule_db[act_name]
+        fact_set = self.fact_db[act_name]            
+            
+    def evalRuleSet(self, rule_set, fact_set):
+        """Evaluates rule set and returns a performance metric tuple."""
+        pass
+        
 if __name__ == '__main__':
     rospy.init_node('rule_manager')
     rule_manager = RuleManager()
