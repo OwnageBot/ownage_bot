60bd5d4e9604e8c14046faba62e4167d9cbf3543
Tan Zong Xuan <tanqazx@gmail.com>
2017-08-17 16:43:56 -0400

    Dubious improvements to command prompt.

--- nodes/dialog_manager.py
+++ nodes/dialog_manager.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import rospy
-import nltk
 from std_msgs.msg import String
 from geometry_msgs.msg import Point
 from ownage_bot import *
@@ -16,16 +15,12 @@ class DialogManager:
         self.task_db = dict(zip([t.name for t in tasks.db], tasks.db))
         
         # Subscribers and publishers
-        self.input_sub = rospy.Subscriber("text_input", String, self.inputCb)
+        self.input_sub = rospy.Subscriber("dialog_in", String, self.inputCb)
+        self.output_pub = rospy.Publisher("dialog_out", String, queue_size=10)
         self.command_pub = rospy.Publisher("command", TaskMsg, queue_size=10)
 
-        # Sentence type will be determined by the PoS of the initial word
-        self.q_words_PoS = ["WP", "WP$", "WDT"]  # For questions
-        self.c_words_PoS = ["VB"]  # For commands
-        self.s_words_PoS = ["IN"]  # For statements
-
     def inputCb(self, msg):
-        """Handles text input and publishes commands."""
+        """Handles dialog input and publishes commands."""
         name = tasks.Idle.name
 
         args = msg.data.split()
@@ -34,9 +29,8 @@ class DialogManager:
             return
         if args[0] == "list":
             # List available actions
-            print "Available actions:"
-            for a in self.action_db.iterkeys():
-                print a
+            out = "\n".join(["Available actions:"] + self.action_db.keys())
+            self.output_pub.publish(out)
             return
         elif args[0] in self.action_db:
             # Try one of the higher-level tasks
@@ -52,36 +46,9 @@ class DialogManager:
             self.command_pub.publish(cmd)
             return
         else:
-            self.handleNatLang(args)
+            pass
         if name == tasks.Idle.name:
             print "Could not parse input, defaulting to idle task."
-
-    def handleNatLang(self, args):
-        pos_tag_args = nltk.pos_tag(args)
-        if pos_tag_args[0][1] in self.c_words_PoS:
-            # Expects command of the form:
-            # "if x true and owns a b true then z false"
-
-            # Chunks predicates and action
-
-            grammar = r"""
-            PREDS: {<.*>+}
-                    }<RB|IN|CC>{
-            """
-            cp = nltk.RegexpParser(grammar)
-            result = cp.parse(pos_tag_args)
-            preds = [] # list of list of predicates
-            action = [] # Action associated w predicate
-            print(result)
-            for i in result.subtrees():
-                if i.label() == 'PREDS':
-                    chunks = nltk.chunk.tree2conlltags(i)
-                    pred = [p[0] for p in chunks if "and" not in p]
-                    preds.append(pred)
-            action = preds.pop()
-            print(action)
-            # TODO Update predicate/rule db
-            return
         
 if __name__ == '__main__':
     rospy.init_node('dialog_manager')
