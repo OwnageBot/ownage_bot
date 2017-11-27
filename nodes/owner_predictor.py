1cd01fc24c6b7c9f416b7e2ed658cb648ea497cc
Tan Zong Xuan <tanqazx@gmail.com>
2017-11-27 16:53:26 -0500

    Adde ownership prediction from percepts via kernel logistic regression.

--- nodes/owner_predictor.py
+++ nodes/owner_predictor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import rospy
-from collections import namedtuple
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics.pairwise import rbf_kernel
 from std_srvs.srv import Trigger, TriggerResponse
 from ownage_bot import *
 from ownage_bot.msg import *
@@ -20,9 +22,22 @@ class OwnerPredictor:
         # Publisher for ownership predictions
         self.owner_pub = rospy.Publisher("owner_prediction", ObjectMsg,
                                          queue_size=10)
+        
         # Client for looking up active rules
         self.lookupRules = rospy.ServiceProxy("lookup_rules", LookupRules)
+        # Client for looking up tracked objects
+        self.listObjects = rospy.ServiceProxy("list_objects", ListObjects)
 
+        # Weights for perceptual properties in distance metric
+        self.w_color = rospy.get_param("~w_color", 30)
+        self.w_pos = rospy.get_param("~w_pos", 10)
+        self.w_matrix = np.diag([self.w_color] + [self.w_pos] * 3)
+
+        # Logistic regression params and vars for percept-based prediction
+        self.regu_strength = 1.0
+        self.lr_classifier = LogisticRegression(C=1/self.regu_strength,
+                                                fit_intercept=False)
+        
     def permInputCb(self, msg):
         """Callback upon receiving permission information about objects."""
         # Ignore perms which are not about actions
@@ -37,6 +52,8 @@ class OwnerPredictor:
             return
         obj = Object.fromStr(msg.bindings[0])
 
+        # TODO: Should repeated permissions be ignored??
+        
         # Guess ownership and publish prediction
         ownership = self.guessFromPerm(action, obj, msg.truth)
         obj.ownership = ownership
@@ -93,4 +110,37 @@ class OwnerPredictor:
 
     def guessFromPercepts(self, obj):
         """Guess ownership from perception of physical properties."""
-        return obj.ownership
+        objs = self.listObjects().objects
+
+        # Compute kernel for kernel logistic regression
+        kern_mat = self.perceptKern(objs, objs)
+        # Tile the matrix to account for uncertainty in labels
+        kern_mat = np.tile(kern_mat, (2,2))
+        
+        # Train a kernel logistic classifier for each possible owner
+        ownership = dict()
+        for a in Agent.universe():
+            weights = np.array([o.ownership[a.id] for o in objs] +
+                               [1.0-o.ownership[a.id] for in objs])
+            classes = [w >= 0.5 for w in weights]
+            self.lr_classifier.fit(kern_mat, classes, sample_weight=weights)
+            # Predict ownership of new object
+            kern_features = self.perceptKern([obj], objs)
+            probs = self.lr_classifier.predict_proba(kern_features)
+            ownership[a.id] = probs[0,0]
+        
+        return ownership
+
+    def perceptDiff(self, o1, o2):
+        """Computes raw displacement in perceptual space between objects."""
+        col_diff = 1.0 if o1.color != o2.color else 0.0
+        p1, p2 = o1.position, o2.position
+        pos_diff = [p1.x-p2.x, p1.y-p2.y, p1.z-p2.z]
+        return np.array([col_diff] + pos_diff)
+
+    def perceptKern(self, objs1, objs2, gamma=1.0):
+        """Computes RBF kernel matrix for the percept features of objects."""
+        diffs = [[self.perceptDiff(o1, o2) for o2 in objs2] for o1 in objs1]
+        sq_dists = np.array([[np.dot(d, d) for d in row] for row in diffs])
+        return np.exp(-gamma * sq_dists)
+        
